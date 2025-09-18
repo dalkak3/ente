f91c3ab8711ef7f47305aa639dedec781c4c94d6
Gnlow <gnlow@pm.me>
2025-09-18 15:29:56 +0900

    Update mod.ts

--- case/mod.ts
+++ case/mod.ts
@@ -1,5 +1,5 @@
-export * as fib from "./json/fib.json" with { type: "json" }
-export * as fib2 from "./json/fib2.json" with { type: "json" }
-export * as memoInFun from "./json/memoInFun.json" with { type: "json" }
-export * as proj1 from "./json/proj1.json" with { type: "json" }
-export * as proj2 from "./json/proj2.json" with { type: "json" }
+export { default as fib } from "./json/fib.json" with { type: "json" }
+export { default as fib2 } from "./json/fib2.json" with { type: "json" }
+export { default as memoInFun } from "./json/memoInFun.json" with { type: "json" }
+export { default as proj1 } from "./json/proj1.json" with { type: "json" }
+export { default as proj2 } from "./json/proj2.json" with { type: "json" }
