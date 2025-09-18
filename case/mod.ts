d6ee28b2a4fbd043ed7740a83ca9d1ae1a2ace14
Gnlow <gnlow@pm.me>
2025-09-18 15:21:36 +0900

    init

---
+++ case/mod.ts
@@ -0,0 +1,5 @@
+export * from "./json/fib.json" with { type: "json" }
+export * from "./json/fib2.json" with { type: "json" }
+export * from "./json/memoInFun.json" with { type: "json" }
+export * from "./json/proj1.json" with { type: "json" }
+export * from "./json/proj2.json" with { type: "json" }
