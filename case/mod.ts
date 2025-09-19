3348493102eb48a3410864b6457b6b4564f7babc
Gnlow <gnlow@pm.me>
2025-09-19 12:27:59 +0900

    add case

--- case/mod.ts
+++ case/mod.ts
@@ -1,5 +1,6 @@
 export { default as proj_57d79d29a76b6b017780b483 } from "./json/57d79d29a76b6b017780b483.json" with { type: "json" }
 export { default as proj_652371adb7aa680135cc2070 } from "./json/652371adb7aa680135cc2070.json" with { type: "json" }
+export { default as proj_68ccc8a640fd193bc2f86363 } from "./json/68ccc8a640fd193bc2f86363.json" with { type: "json" }
 export { default as fib } from "./json/fib.json" with { type: "json" }
 export { default as fib2 } from "./json/fib2.json" with { type: "json" }
 export { default as memoInFun } from "./json/memoInFun.json" with { type: "json" }
